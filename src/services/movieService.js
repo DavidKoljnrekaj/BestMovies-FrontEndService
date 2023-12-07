c7503302a8baf63adc9c9b9ddc5426f4e6d2ed61
DavidKoljnrekaj <koljnrekajdavid@gmail.com>
2023-12-07 15:26:21 +0100

    deployment fix?

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://34.88.186.82/movies';// 'http://localhost:5001/movies';
+const API_BASE_URL = 'http://34.88.186.82/movies'; //'http://localhost:5001/movies';
 let fetch;
 
 if (typeof window === 'undefined') {
@@ -41,7 +41,6 @@ export const getMovieList = async (type/* ,page*/) => {
 export const getTrendingMovies = async () => {
   try {
     const response = await fetch(`${API_BASE_URL}/trending/today`);
-    console.log(response);
     const data = await response.json();
     return data;
   } catch (error) {

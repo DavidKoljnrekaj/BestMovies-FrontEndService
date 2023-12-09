5214715d742bdd667c43dec3f9890c92c8034962
DavidKoljnrekaj <koljnrekajdavid@gmail.com>
2023-12-09 13:48:05 +0100

    genre icons

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -3,10 +3,33 @@ import { Link } from 'react-router-dom';
 import MovieTable from '../components/MovieTable';
 import './Trending.js.css';
 import MovieCard from './MovieCard';
+//genre icons
+import action from '../icons/action.jpg';
+import biography from '../icons/biography.jpg';
+import comedy from '../icons/comedy.jpg';
+import detective from '../icons/detective.jpg';
+import drama from '../icons/drama.jpg';
+import fantasy from '../icons/fantasy.jpg';
+import history from '../icons/history.jpg';
+import horror from '../icons/horror.jpg';
+import musical from '../icons/musical.jpg';
+import mystery from '../icons/mystery.jpg';
+import romance from '../icons/romance.jpg';
+import scienceFiction from '../icons/science-fiction.jpg';
+import sport from '../icons/sport.jpg';
+import thriller from '../icons/thriller.jpg';
+import war from '../icons/war.jpg';
+import western from '../icons/western.jpg';
+const genreImages = {
+  'Action': action,'Biography' : biography,'Comedy' : comedy,'Detective' : detective,'Drama' : drama,'Fantasy' : fantasy,'History' : history,'Horror' : horror,
+  'Musical' : musical,'Mystery' : mystery,'Romance' : romance,'Science Fiction' : scienceFiction,'Sport' : sport,'Thriller' : thriller,'War' : war,'Western' : western,
+};
 const BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/original';
 
 function Trending({ trendingMovies, highestRatedMovie, mostPopularGenre, mostFrequentActor }) {
     const posterUrl = `${BASE_IMAGE_URL}${mostFrequentActor.profile_path}`;
+    const genreIcon = genreImages[mostPopularGenre];
+
     return (
       <>
         <h2 className="trending-title">Trending today:</h2>
@@ -20,7 +43,7 @@ function Trending({ trendingMovies, highestRatedMovie, mostPopularGenre, mostFre
           </div>
           <div className="statistic-item">
             <h3>Most Popular Genre Today</h3>
-            <p>{mostPopularGenre}</p>
+            <img src={genreIcon} alt={mostPopularGenre} />
           </div>
           <div className="statistic-item">
             <h3>Most Frequent Actor in Trending</h3>
